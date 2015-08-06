33a31868e040ffc1c4b415816e366cc903f2ff60
Navnish Bhardwaj <navnishb@gmail.com>
2015-08-06 18:38:42 +0530

    Create run.php
    Main file where user will enter list of URLs (URI) through textarea

---
+++ run.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form action="ex.php" method="POST">
+Enter URL(URI):<br/>
+<textarea rows="8" cols="30" name="t1"></textarea>
+<input type="submit" name="submit" value="Check">
+
+
+</form>
+</body>
+</html>
